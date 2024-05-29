a62fb3c54785a841df241d78d1649fe9ff2dfbfa
Yogi-Choudhary <yogichoudhary9715@gmail.com>
2024-05-29 15:10:50 +0530

    show particular recipe

--- app/javascript/routes/index.jsx
+++ app/javascript/routes/index.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Home from "../components/Home";
 import Recipes from "../components/Recipes";
+import Recipe from "../components/Recipe";
 
 
 export default (
@@ -9,6 +10,7 @@ export default (
     <Routes>
       <Route path="/" element={<Home />} />
       <Route path="/recipes" element={<Recipes />} />
+      <Route path="/recipe/:id" element={<Recipe />} />
     </Routes>
   </Router>
 );
